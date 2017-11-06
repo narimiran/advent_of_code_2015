c55903dd69c9644bd5b179d1fc6f56b346ebfb8a
narimiran <miran.tuhtan+git@gmail.com>
2017-11-06 17:56:35 +0100

    day 06

--- day06.nim
+++ day06.nim
@@ -1,66 +1,65 @@
-import strutils, sequtils, math
-import nimprof
+    import strutils, sequtils, math
 
-const instructions = readFile("./inputs/06.txt").strip.splitLines
+    const instructions = readFile("./inputs/06.txt").strip.splitLines
 
-type Grid = array[1000, array[1000, int]]
+    type Grid = array[1000, array[1000, int]]
 
-var
-  grid1: Grid
-  grid2: Grid
-
-
-proc setValues(value: int, start, stop: seq) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid1[row][col] = value
-
-proc toggle(start, stop: seq) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid1[row][col] *= -1
-
-proc totalOn(grid: Grid): int =
-  for i in 0 .. 999:
-    for j in 0 .. 999:
-      if grid[i][j] == 1:
-        inc result
-
-proc increaseBrightness(start, stop: seq, value=1) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid2[row][col] += value
-
-proc decreaseBrightness(start, stop: seq) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid2[row][col] = max(grid2[row][col] - 1, 0)
+    var
+      grid1: Grid
+      grid2: Grid
 
 
-setValues(-1, @[0, 0], @[999, 999])
+    proc setValues(value: int, start, stop: seq) =
+      for row in start[0] .. stop[0]:
+        for col in start[1] .. stop[1]:
+          grid1[row][col] = value
 
-for line in instructions:
-  let
-    commands = line.splitWhitespace
-    start = commands[^3].split(',').map(parseInt)
-    stop = commands[^1].split(',').map(parseInt)
+    proc toggle(start, stop: seq) =
+      for row in start[0] .. stop[0]:
+        for col in start[1] .. stop[1]:
+          grid1[row][col] *= -1
 
-  case commands[1]
-  of "on":
-    setValues(1, start, stop)
-    increaseBrightness(start, stop)
-  of "off":
-    setValues(-1, start, stop)
-    decreaseBrightness(start, stop)
-  else:
-    toggle(start, stop)
-    increaseBrightness(start, stop, 2)
+    proc totalOn(grid: Grid): int =
+      for i in 0 .. 999:
+        for j in 0 .. 999:
+          if grid[i][j] == 1:
+            inc result
+
+    proc increaseBrightness(start, stop: seq, value=1) =
+      for row in start[0] .. stop[0]:
+        for col in start[1] .. stop[1]:
+          grid2[row][col] += value
+
+    proc decreaseBrightness(start, stop: seq) =
+      for row in start[0] .. stop[0]:
+        for col in start[1] .. stop[1]:
+          grid2[row][col] = max(grid2[row][col] - 1, 0)
 
 
-echo grid1.totalOn()
+    setValues(-1, @[0, 0], @[999, 999])
 
-var brightness: int
-for row in grid2:
-  brightness += sum(row)
+    for line in instructions:
+      let
+        commands = line.splitWhitespace
+        start = commands[^3].split(',').map(parseInt)
+        stop = commands[^1].split(',').map(parseInt)
 
-echo brightness
+      case commands[1]
+      of "on":
+        setValues(1, start, stop)
+        increaseBrightness(start, stop)
+      of "off":
+        setValues(-1, start, stop)
+        decreaseBrightness(start, stop)
+      else:
+        toggle(start, stop)
+        increaseBrightness(start, stop, 2)
+
+
+    echo grid1.totalOn()
+
+    var brightness: int
+    for row in grid2:
+      brightness += sum(row)
+
+    echo brightness
