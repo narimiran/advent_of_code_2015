830d27cbd7b36733d00f3302c1928bd8bdb5452d
narimiran <miran.tuhtan+git@gmail.com>
2018-04-16 19:26:24 +0200

    faster day11
    No need for recursive `increment` function.

--- day11.nim
+++ day11.nim
@@ -1,43 +1,41 @@
-proc increment(pw: string): string =
-  if pw[^1] == 'z':
-    return increment(pw[0..^2]) & 'a'
-  else:
-    return pw[0..^2] & chr(ord(pw[^1]) + 1)
+import strutils
 
-proc containsBadCharacters(pw: string, badChars = @['l', 'o', 'i']): bool =
-  for c in badChars:
-    if c in pw:
-      return true
+proc increment(pw: var string) =
+  var i: int
+  while pw[pw.high - i] == 'z':
+    pw[pw.high - i] = 'a'
+    inc i
+  pw[pw.high - i] = chr(ord(pw[pw.high - i]) + 1)
 
 proc hasIncreasingStraight(pw: string): bool =
+  var a, b, c: int
   for i in 0 .. pw.len-2:
-    let
-      a = ord(pw[i])
-      b = ord(pw[i+1]) - 1
-      c = ord(pw[i+2]) - 2
+    a = ord(pw[i])
+    b = ord(pw[i+1]) - 1
+    c = ord(pw[i+2]) - 2
     if a == b and a == c:
       return true
 
 proc hasTwoPairs(pw: string): bool =
-  var pairedChar: set[char] 
+  var pairedChar: set[char]
   for i in 0 .. pw.len-2:
     if pw[i] == pw[i+1]:
       pairedChar.incl(pw[i])
   return card(pairedChar) >= 2
 
 proc isValid(pw: string): bool =
-  return hasIncreasingStraight(pw) and not containsBadCharacters(pw) and hasTwoPairs(pw)
+  return not pw.contains({'l', 'o', 'i'}) and pw.hasIncreasingStraight and pw.hasTwoPairs
 
-proc findValid(pw: string): string =
-  result = pw
-  while not result.isValid():
-    result = increment(result)
+proc findValid(pw: var string) =
+  while not pw.isValid():
+    increment(pw)
 
 
-const password = "cqjxjnds"
+var password = "cqjxjnds"
 
-let first = findValid(password)
-echo first
+findValid(password)
+echo password
 
-let second = findValid(first.increment())
-echo second
+password.increment()
+findValid(password)
+echo password
