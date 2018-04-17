e7578f34dc0ab12ca922547ed0265e40ab23117d
narimiran <miran.tuhtan+git@gmail.com>
2018-04-17 17:24:41 +0200

    faster day13
    Use array instead of table.

--- day13.nim
+++ day13.nim
@@ -1,64 +1,52 @@
-import strutils, tables, algorithm, math
+import strutils, algorithm, math
 
 const instructions = readFile("./inputs/13.txt").strip.splitLines
 
 var
-  people: seq[string] = @[]
-  happinessChart = initTable[string, Table[string, int]]()
+  people = @[0, 1, 2, 3, 4, 5, 6, 7, 8]
+  happinessChart: array[9, array[9, int]]
+  fields: seq[string]
+  name, neighbour: int
+  direction, amount: int
 
 proc gain(a: string): int =
-  if a == "gain":
-    return 1
-  return -1
+  if a == "gain": 1 else: -1
 
+proc parse(c: char): int =
+  let i = ord(c) - ord('A')
+  if i > 6: 7 else: i
 
 for line in instructions:
-  let
-    fields = line.split()
-    name = fields[0]
-    neighbour = fields[^1][0 .. ^2]
-    direction = gain(fields[2])
-    amount = parseInt(fields[3])
-  if not happinessChart.hasKey(name):
-    happinessChart[name] = initTable[string, int]()
+  fields = line.split()
+  name = fields[0][0].parse
+  neighbour = fields[^1][0].parse
+  direction = gain(fields[2])
+  amount = parseInt(fields[3])
   happinessChart[name][neighbour] = direction * amount
 
-for name in happinessChart.keys():
-  people.add(name)
-people.sort(cmp)
 
-
-proc findHappiness(people: seq[string], secondPart=false): seq[int] =
+proc findHappiness(people: seq[int]): seq[int] =
   result = @[]
   let last = people[^1]
   var others = people[0 .. ^2]
-  for _ in 0 .. fac(others.len):
+
+  # remove the obvious permutation duplicates
+  var duplicates = 1
+  for i in 2 .. others.high:
+    duplicates += fac(i)
+
+  for _ in duplicates .. fac(others.len):
     var total: int
     for i, name in others[1 .. ^1]:
       total += happinessChart[name][others[i]]
       total += happinessChart[others[i]][name]
-    if not secondPart:
-      # if secondPart, I'm `last`, bringing 0 happiness
-      total += happinessChart[last][others[^1]]
-      total += happinessChart[others[^1]][last]
-      total += happinessChart[last][others[0]]
-      total += happinessChart[others[0]][last]
+    total += happinessChart[last][others[^1]]
+    total += happinessChart[others[^1]][last]
+    total += happinessChart[last][others[0]]
+    total += happinessChart[others[0]][last]
     result.add(total)
     others.nextPermutation()
-  result.sort(cmp)
 
 
-let first = findHappiness(people)[^1]
-echo first
-
-
-people.add("me")
-happinessChart["me"] = initTable[string, int]()
-
-for name in people:
-  happinessChart["me"][name] = 0
-  happinessChart[name]["me"] = 0
-people.sort(cmp)
-
-let second = findHappiness(people, true)[^1]
-echo second
+echo findHappiness(people[0 ..< people.high]).max
+echo findHappiness(people).max
