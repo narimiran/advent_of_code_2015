e38c11e7450fd0334f501919890ae0043811f980
narimiran <miran.tuhtan+git@gmail.com>
2018-05-05 10:22:03 +0200

    parallel day04
    Search for hashes in parallel.
    Reduces previous runtime of ~1s to ~0.2s.

--- day04.nim
+++ day04.nim
@@ -1,25 +1,41 @@
-import md5, strutils
+import md5, strutils, sequtils
+import threadpool
+{.experimental.}
 
-const salt = "ckczppom"
+
+const
+  salt = "ckczppom"
+  batchSize = 100_000
 
 
-proc findIndex(): tuple[first, second: int] =
+template firstCondition(hash: MD5Digest): bool =
+  hash[0] == 0 and hash[1] == 0 and hash[2] < 16
+
+template secondCondition(hash: MD5Digest): bool =
+  hash[0] == 0 and hash[1] == 0 and hash[2] == 0
+
+
+proc helper(start: int, isFirst: bool): int =
   var
-    i, first, second: int
-    foundFirst: bool
-  while true:
-    let
-      s = salt & intToStr(i)
-      hash = s.toMD5
-    if hash[0] == 0 and hash[1] == 0 and hash[2] < 16:
-      if not foundFirst:
-        first = i
-        foundFirst = true
-      if hash[2] == 0:
-        second = i
-        break
-    inc i
-  return (first, second)
+    s: string
+    hash: MD5Digest
+  for i in start ..< start + batchSize:
+    s = salt & intToStr(i)
+    hash = s.toMD5
+    if isFirst and firstCondition(hash):
+      return i
+    elif secondCondition(hash):
+      return i
 
+proc solve(): tuple[first, second: int] =
+  var
+    first = newSeq[int](2)
+    second = newSeq[int](40)
+  parallel:
+    for i in 0 .. first.high:
+      first[i] = spawn helper(i * batchSize, true)
+    for i in 1 .. second.high:
+      second[i] = spawn helper(i * batchSize, false)
+  result = (first.filterIt(it > 0).min, second.filterIt(it > 0).min)
 
-echo findIndex()
+echo solve()
