0aea8883384984ea403203f48194a1dd963da155
narimiran <narimiran@disroot.org>
2018-10-06 19:42:38 +0200

    take advatage of templates
    * simplified, more readable, procs
    * use `strscans`

--- day06.nim
+++ day06.nim
@@ -1,65 +1,62 @@
-import strutils, sequtils, math
+import strutils, strscans
 
-const instructions = readFile("./inputs/06.txt").strip.splitLines
+const instructions = readFile("./inputs/06.txt").strip.splitLines()
 
-type Grid = array[1000, array[1000, int]]
+type
+  Grid = array[1000, array[1000, int]]
+  Subgrid = tuple[x1, y1, x2, y2: int]
+
+var grid1, grid2: Grid
+
+
+template forSubgrid(sg: Subgrid, body: untyped) =
+  for x {.inject.} in sg.x1 .. sg.x2:
+    for y {.inject.} in sg.y1 .. sg.y2:
+      body
+
+
+proc setValue(grid: var Grid, sg: Subgrid, value: int) =
+  forSubgrid sg:
+    grid[x][y] = value
+
+proc toggle(grid: var Grid, sg: Subgrid) =
+  forSubgrid sg:
+    grid[x][y] = 1 - grid[x][y]
+
+proc increaseBrightness(grid: var Grid, sg: Subgrid, value: int) =
+  forSubgrid sg:
+    grid[x][y] += value
+
+proc decreaseBrightness(grid: var Grid, sg: Subgrid) =
+  forSubgrid sg:
+    grid[x][y] = max(grid[x][y] - 1, 0)
+
+func lights(grid: Grid): int =
+  for row in grid:
+    for value in row:
+      result += value
+
 
 var
-  grid1: Grid
-  grid2: Grid
-
-
-proc setValues(value: int, start, stop: seq) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid1[row][col] = value
-
-proc toggle(start, stop: seq) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid1[row][col] *= -1
-
-proc totalOn(grid: Grid): int =
-  for i in 0 .. 999:
-    for j in 0 .. 999:
-      if grid[i][j] == 1:
-        inc result
-
-proc increaseBrightness(start, stop: seq, value=1) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid2[row][col] += value
-
-proc decreaseBrightness(start, stop: seq) =
-  for row in start[0] .. stop[0]:
-    for col in start[1] .. stop[1]:
-      grid2[row][col] = max(grid2[row][col] - 1, 0)
-
-
-setValues(-1, @[0, 0], @[999, 999])
+  x1, y1, x2, y2: int
+  command: string
+  sg: Subgrid
 
 for line in instructions:
-  let
-    commands = line.splitWhitespace
-    start = commands[^3].split(',').map(parseInt)
-    stop = commands[^1].split(',').map(parseInt)
-
-  case commands[1]
-  of "on":
-    setValues(1, start, stop)
-    increaseBrightness(start, stop)
-  of "off":
-    setValues(-1, start, stop)
-    decreaseBrightness(start, stop)
-  else:
-    toggle(start, stop)
-    increaseBrightness(start, stop, 2)
+  if line.scanf("turn $w $i,$i through $i,$i", command, x1, y1, x2, y2):
+    sg = (x1, y1, x2, y2)
+    case command
+      of "on":
+        grid1.setValue(sg, 1)
+        grid2.increaseBrightness(sg, 1)
+      of "off":
+        grid1.setValue(sg, 0)
+        grid2.decreaseBrightness(sg)
+  elif line.scanf("toggle $i,$i through $i,$i", x1, y1, x2, y2):
+    sg = (x1, y1, x2, y2)
+    grid1.toggle(sg)
+    grid2.increaseBrightness(sg, 2)
 
 
-echo grid1.totalOn()
-
-var brightness: int
-for row in grid2:
-  brightness += sum(row)
-
-echo brightness
+echo grid1.lights()
+echo grid2.lights()
