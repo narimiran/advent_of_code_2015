c0099297e3d3536a1e504e459b4db434ad3f4494
narimiran <narimiran@disroot.org>
2018-10-07 12:17:22 +0200

    unpack each line only once, use array instead of table

--- day23.nim
+++ day23.nim
@@ -1,33 +1,41 @@
-import strutils, tables
+import strutils
 
-const instructions = readFile("./inputs/23.txt").splitLines()
+type Commands = tuple[instr: string, reg, jump: int]
+
+const instructions = block:
+  var res = newSeq[Commands]()
+  var lines = readFile("./inputs/23.txt").splitLines()
+  for line in lines:
+    let
+      commands = line.split()
+      instr = commands[0]
+      reg = ord(commands[1][0]) - ord('a')
+      jump = if instr.startsWith('j'): commands[^1].parseInt-1 else: 0
+    res.add((instr, reg, jump))
+  res
 
 
-proc execute(a: int): int =
+func execute(a: int): int =
   var
-    registers = {'a': a, 'b': 0}.toTable()
-    i = 0
+    registers = [a, 0]
+    i: int
 
   while i < len(instructions):
-    let
-      commands = instructions[i].split()
-      instr = commands[0]
-      reg = commands[1][0]
-      jump = if instr.startsWith('j'): commands[^1].parseInt-1 else: 0
-    
+    let (instr, reg, jump) = instructions[i]
+
     case instr
-    of "inc": inc registers[reg]
-    of "hlf": registers[reg] = registers[reg] div 2
-    of "tpl": registers[reg] = 3 * registers[reg]
-    of "jmp": i += jump
-    of "jio":
-      if registers[reg] == 1: 
-        i += jump
-    of "jie":
-      if registers[reg] mod 2 == 0:
-        i += jump
+      of "inc": inc registers[reg]
+      of "hlf": registers[reg] = registers[reg] div 2
+      of "tpl": registers[reg] = 3 * registers[reg]
+      of "jmp": i += jump
+      of "jio":
+        if registers[reg] == 1:
+          i += jump
+      of "jie":
+        if registers[reg] mod 2 == 0:
+          i += jump
     inc i
-  return registers['b']
+  return registers[1]
 
 
 echo execute(a=0)
