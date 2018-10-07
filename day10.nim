f3c8299e34fa0b6dc884b6c220a377a09ef8b93e
narimiran <narimiran@disroot.org>
2018-10-07 12:10:46 +0200

    use `int8`: twice as fast, 7x less memory usage

--- day10.nim
+++ day10.nim
@@ -1,13 +1,12 @@
-var digits = @[3, 1, 1, 3, 3, 2, 2, 1, 1, 3]
+var digits = @[3'i8, 1, 1, 3, 3, 2, 2, 1, 1, 3]
 
-proc say(word: seq[int]): seq[int] =
-  result = @[]
+func say(word: seq[int8]): seq[int8] =
   var
     a = word[0]
-    repeats = 1
+    repeats = 1'i8
 
   for c in word[1 .. word.high]:
-    if c == a: repeats += 1
+    if c == a: inc repeats
     else:
       result.add(repeats)
       result.add(a)
@@ -16,7 +15,7 @@ proc say(word: seq[int]): seq[int] =
   result.add(repeats)
   result.add(a)
 
-proc repeat(word: var seq[int], times: int) =
+func repeat(word: var seq[int8], times: int) =
   for _ in 1 .. times:
     word = word.say()
 
